c5acdb68237ab57b72969be13e759ff1403ec12b
ryan-kim-dev <ryankim.h.dev@gmail.com>
2023-01-22 21:15:26 +0900

    Feat: router link, router view App.vue에 등록

---
+++ vue2/endgame/vue-til-client/src/views/LoginPage.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>Login</div>
+</template>
+
+<script>
+export default {
+  name: 'LoginPage',
+
+  data() {
+    return {};
+  },
+
+  mounted() {},
+
+  methods: {},
+};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ vue2/endgame/vue-til-client/src/views/SignupPage.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>Signup</div>
+</template>
+
+<script>
+export default {
+  name: 'SignupPage',
+
+  data() {
+    return {};
+  },
+
+  mounted() {},
+
+  methods: {},
+};
+</script>
+
+<style lang="scss" scoped></style>
