a84a31c3fd5c807464973ab38a4bdf7163780971
ryan-kim-dev <ryankim.h.dev@gmail.com>
2023-01-23 15:33:05 +0900

    Feat: 라우터 history 모드 적용, 404페이지 추가

--- vue2/endgame/vue-til-client/src/routes/index.js
+++ vue2/endgame/vue-til-client/src/routes/index.js
@@ -7,7 +7,12 @@ import VueRouter from 'vue-router';
 Vue.use(VueRouter);
 
 export default new VueRouter({
+  mode: 'history',
   routes: [
+    {
+      path: '/',
+      redirect: '/login',
+    },
     {
       path: '/login',
       component: () => import('@/views/LoginPage.vue'),
@@ -15,6 +20,10 @@ export default new VueRouter({
     {
       path: '/signup',
       component: () => import('@/views/SignupPage.vue'),
+    },
+    {
+      path: '*',
+      component: () => import('@/views/NotFoundPage.vue'),
     },
   ],
 });
---
+++ vue2/endgame/vue-til-client/src/views/NotFoundPage.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>페이지를 찾을 수 없습니다.</div>
+</template>
+
+<script>
+export default {
+  name: 'NotFoundPage',
+
+  data() {
+    return {};
+  },
+
+  mounted() {},
+
+  methods: {},
+};
+</script>
+
+<style lang="scss" scoped></style>
